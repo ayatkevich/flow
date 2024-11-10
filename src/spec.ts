ca872782b212385051677efaaa4bca45fedb80a1
Alex Yatkevich <ayatkevich@users.noreply.github.com>
2024-11-10 15:36:24 +0700

    add an example

--- src/spec.ts
+++ src/spec.ts
@@ -81,6 +81,161 @@ describe("tracify", () => {
     verify(IO, io);
   });
 
+  test("example - cli tool", async () => {
+    const env = fn("env");
+    const getPackageJson = fn("getPackageJson");
+    const readFile = fn("readFile");
+    const writeFile = fn("writeFile");
+    const gitStatus = fn("gitStatus");
+    const openai = fn("openai");
+
+    const incorrectOpenaiResponse = openai
+      .takes({
+        apiKey: "sk-1234567890",
+        model: "gpt-4o",
+        messages: [
+          { role: "system", content: "You decide what version to bump to" },
+          {
+            role: "user",
+            content: [
+              `App name: my-app`,
+              `App version: 1.0.0`,
+              `Git status: ${JSON.stringify({
+                staged: [{ path: "src/index.ts" }],
+                unstaged: [],
+                untracked: [],
+              })}`,
+              `src/index.ts:`,
+              `export function main() {}`,
+              `What version should we bump to?`,
+            ].join("\n"),
+          },
+        ],
+      })
+      .returns("Incorrect response");
+
+    const correctOpenaiResponse = openai
+      .takes({
+        apiKey: "sk-1234567890",
+        model: "gpt-4o",
+        messages: [
+          { role: "system", content: "You decide what version to bump to" },
+          {
+            role: "user",
+            content: [
+              `App name: my-app`,
+              `App version: 1.0.0`,
+              `Git status: ${JSON.stringify({
+                staged: [{ path: "src/index.ts" }],
+                unstaged: [],
+                untracked: [],
+              })}`,
+              `src/index.ts:`,
+              `export function main() {}`,
+              `What version should we bump to?`,
+            ].join("\n"),
+          },
+        ],
+      })
+      .returns("1.1.0");
+
+    const IO = program([
+      trace([
+        yields(env.takes("OPENAI_API_KEY").returns("sk-1234567890")),
+        yields(getPackageJson.takes().returns({ name: "my-app", version: "1.0.0" })),
+        yields(gitStatus.takes().returns({ staged: [], unstaged: [], untracked: [] })),
+        returns(undefined),
+      ]),
+
+      trace([
+        yields(env.takes("OPENAI_API_KEY").returns("sk-1234567890")),
+        yields(getPackageJson.takes().returns({ name: "my-app", version: "1.0.0" })),
+        yields(
+          gitStatus
+            .takes()
+            .returns({ staged: [{ path: "src/index.ts" }], unstaged: [], untracked: [] })
+        ),
+        yields(readFile.takes("src/index.ts").returns("export function main() {}")),
+        yields(correctOpenaiResponse),
+        yields(
+          writeFile.takes("package.json", { name: "my-app", version: "1.1.0" }).returns(undefined)
+        ),
+      ]),
+
+      trace([
+        yields(env.takes("OPENAI_API_KEY").returns("sk-1234567890")),
+        yields(getPackageJson.takes().returns({ name: "my-app", version: "1.0.0" })),
+        yields(
+          gitStatus
+            .takes()
+            .returns({ staged: [{ path: "src/index.ts" }], unstaged: [], untracked: [] })
+        ),
+        yields(readFile.takes("src/index.ts").returns("export function main() {}")),
+        yields(incorrectOpenaiResponse),
+        yields(correctOpenaiResponse),
+        yields(
+          writeFile.takes("package.json", { name: "my-app", version: "1.1.0" }).returns(undefined)
+        ),
+      ]),
+
+      trace([
+        yields(env.takes("OPENAI_API_KEY").returns("sk-1234567890")),
+        yields(getPackageJson.takes().returns({ name: "my-app", version: "1.0.0" })),
+        yields(
+          gitStatus
+            .takes()
+            .returns({ staged: [{ path: "src/index.ts" }], unstaged: [], untracked: [] })
+        ),
+        yields(readFile.takes("src/index.ts").returns("export function main() {}")),
+        yields(incorrectOpenaiResponse),
+        yields(incorrectOpenaiResponse),
+        yields(incorrectOpenaiResponse),
+        throws(new Error("Three attempts failed")),
+      ]),
+    ]);
+
+    const io = implementation(IO, function* () {
+      const apiKey = yield* this.env("OPENAI_API_KEY");
+      const { name: appName, version: appVersion } = yield* this.getPackageJson();
+      const gitStatus = yield* this.gitStatus();
+
+      if (gitStatus.staged.length === 0) return;
+
+      const fileContents = [];
+      for (const file of gitStatus.staged) {
+        const contents = yield* this.readFile(file.path);
+        fileContents.push(`${file.path}:`, contents);
+      }
+
+      let retryCount = 0;
+      do {
+        var response = yield* this.openai({
+          apiKey,
+          model: "gpt-4o",
+          messages: [
+            { role: "system", content: "You decide what version to bump to" },
+            {
+              role: "user",
+              content: [
+                `App name: ${appName}`,
+                `App version: ${appVersion}`,
+                `Git status: ${JSON.stringify(gitStatus)}`,
+                ...fileContents,
+                `What version should we bump to?`,
+              ].join("\n"),
+            },
+          ],
+        });
+      } while (!response.match(/^[0-9]+\.[0-9]+\.[0-9]+$/) && ++retryCount < 3);
+
+      if (retryCount >= 3) throw new Error("Three attempts failed");
+
+      yield* this.writeFile("package.json", { name: appName, version: response });
+    });
+
+    verify(IO, io);
+  });
+
   it("should not allow use of effects that were not defined in the program", () => {
     const IO = program([]);
     const io = implementation(IO, function* () {
